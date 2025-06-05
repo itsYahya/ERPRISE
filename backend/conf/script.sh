22f520547dfa5b632920841157ee4d8b4399b761
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-06-06 00:15:22 +0100

    fix: adding a wait for postgres db before the backend server start

--- backend/conf/script.sh
+++ backend/conf/script.sh
@@ -3,6 +3,6 @@
 chmod +x wait-for-it.sh
 
 ./wait-for-it.sh postgres:5432 -- python api/manage.py makemigrations
-python api/manage.py migrate
+./wait-for-it.sh postgres:5432 -- python api/manage.py migrate
 
-python api/manage.py runserver 0.0.0.0:8000
+./wait-for-it.sh postgres:5432 -- python api/manage.py runserver 0.0.0.0:8000
