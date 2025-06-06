c5da7f5f9a3c19471a7922f14b0cc5d7f8b18dd1
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-06-06 01:03:06 +0100

    feat: adding job, department and salary serializers

--- backend/src/humanResources/serializers.py
+++ backend/src/humanResources/serializers.py
@@ -1,7 +1,29 @@
 from rest_framework import serializers
-from .models import Employee
+from .models import Employee, Department, Job, Salary
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = '__all__'
+
+
+class SalarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Salary
+        fields = '__all__'
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    department = DepartmentSerializer(read_only=True)
+    job = JobSerializer(read_only=True)
+    salaries = SalarySerializer(many=True, read_only=True)
+
     class Meta:
         model = Employee
         fields = '__all__'
