5b3b25c02d409b50bafb1347fcfd0c4703d677a7
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-06-06 01:05:12 +0100

    feat: adding urls for the added controllers

--- backend/src/humanResources/urls.py
+++ backend/src/humanResources/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet
+from .views import EmployeeViewSet, JobViewSet, DepartmentViewSet, SalaryViewSet
 
 router = DefaultRouter()
 router.register(r'employees', EmployeeViewSet, basename='employee')
+router.register(r'job', JobViewSet, basename='job')
+router.register(r'department', DepartmentViewSet, basename='department')
+router.register(r'salary', SalaryViewSet, basename='salary')
 
 urlpatterns = [
     path('', include(router.urls)),
