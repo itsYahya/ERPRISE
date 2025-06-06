c77bb250e0ddac43d8982e1164d18757cf967430
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-06-06 01:03:51 +0100

    feat: adding views for the job , department and salary controllers

--- backend/src/humanResources/views.py
+++ backend/src/humanResources/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import Employee
-from .serializers import EmployeeSerializer
+from .models import Employee, Job, Department, Salary
+from .serializers import EmployeeSerializer, JobSerializer, DepartmentSerializer, SalarySerializer
 from rest_framework.permissions import IsAuthenticated
 
 class EmployeeViewSet(viewsets.ModelViewSet):
@@ -8,3 +8,17 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     serializer_class = EmployeeSerializer
     #permission_classes = [IsAuthenticated] 
 
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    #permission_classes = [IsAuthenticated] 
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    #permission_classes = [IsAuthenticated] 
+
+class SalaryViewSet(viewsets.ModelViewSet):
+    queryset = Salary.objects.all()
+    serializer_class = SalarySerializer
+    #permission_classes = [IsAuthenticated]
