7f48fe04f94b2f4ef705e38e0b80cd47eb03a8a9
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-06-09 20:49:20 +0100

    feat: create views for crm serializers

--- backend/src/crm/views.py
+++ backend/src/crm/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Client, Contact
+from .serializers import ClientSerializer, ContactSerializer
 
-# Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
