2e6896ea0163d151c1421ef20464aa5d0b981258
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-06-09 20:50:24 +0100

    feat: create views for products models

--- backend/src/products/views.py
+++ backend/src/products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, Inventory, Order, OrderItem
+from .serializers import ProductSerializer, InventorySerializer, OrderSerializer, OrderItemSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class InventoryViewSet(viewsets.ModelViewSet):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
